d79997c78f7097e41600abe4ef2f03b9a0000155
Paul Salaets <psalaets@gmail.com>
2015-07-05 15:23:28 -0400

    take tier's k-factor into account when calcing new elo

--- test/elo-test.js
+++ test/elo-test.js
@@ -3,9 +3,39 @@ var elo = require('../lib/elo');
 
 describe('elo', function () {
   it('returns new elo ratings for players', function () {
-    var result = elo(1200, 1200);
+    var winner = {
+      elo: 1200,
+      tier: 1
+    };
+
+    var loser = {
+      elo: 1200,
+      tier: 1
+    };
+
+    var result = elo(winner, loser);
 
     assert(result.winner > 1200);
     assert(result.loser < 1200);
+  });
+
+  it('uses tier to determine player\'s k-factor', function () {
+    var favored = {
+      elo: 1600,
+      tier: 1
+    };
+
+    var underdog = {
+      elo: 900,
+      tier: 4
+    };
+
+    // huge upset
+    var result = elo(underdog, favored);
+
+    // favored didn't drop more than 4
+    assert.equal(1596, result.loser);
+    // underdog didn't gain more than 32
+    assert.equal(931, result.winner);
   });
 });
